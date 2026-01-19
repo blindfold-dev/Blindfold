60bb1dc6573bb818932193637a61abcbead92aa7
Michal Vich <michal.vich@gmail.com>
2026-01-19 20:00:01 +0100

    init documentation

---
+++ packages/js-sdk/src/errors.ts
@@ -0,0 +1,48 @@
+/**
+ * Base error class for Blindfold SDK
+ */
+export class BlindfoldError extends Error {
+  constructor(message: string) {
+    super(message)
+    this.name = 'BlindfoldError'
+    Object.setPrototypeOf(this, BlindfoldError.prototype)
+  }
+}
+
+/**
+ * Error thrown when authentication fails
+ */
+export class AuthenticationError extends BlindfoldError {
+  constructor(message: string = 'Authentication failed. Please check your API key.') {
+    super(message)
+    this.name = 'AuthenticationError'
+    Object.setPrototypeOf(this, AuthenticationError.prototype)
+  }
+}
+
+/**
+ * Error thrown when API request fails
+ */
+export class APIError extends BlindfoldError {
+  statusCode: number
+  responseBody?: any
+
+  constructor(message: string, statusCode: number, responseBody?: any) {
+    super(message)
+    this.name = 'APIError'
+    this.statusCode = statusCode
+    this.responseBody = responseBody
+    Object.setPrototypeOf(this, APIError.prototype)
+  }
+}
+
+/**
+ * Error thrown when network request fails
+ */
+export class NetworkError extends BlindfoldError {
+  constructor(message: string = 'Network request failed. Please check your connection.') {
+    super(message)
+    this.name = 'NetworkError'
+    Object.setPrototypeOf(this, NetworkError.prototype)
+  }
+}
---
+++ packages/js-sdk/src/index.ts
@@ -0,0 +1,15 @@
+export { Blindfold } from './client'
+export type {
+  BlindfoldConfig,
+  TokenizeConfig,
+  TokenizeResponse,
+  DetokenizeResponse,
+  DetectedEntity,
+  APIErrorResponse,
+} from './types'
+export {
+  BlindfoldError,
+  AuthenticationError,
+  APIError,
+  NetworkError,
+} from './errors'
